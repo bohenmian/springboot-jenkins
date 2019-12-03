94a778e884beb1ce4eeef81aaa2c3a8a9dd7096b
bohenmian <bohenmian@gmail.com>
2019-12-03 11:04:59 +0800

    add deploy shell

---
+++ ci/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+cat ci/docker-compose.yml | sed "s/@@HOST@@/$HOST/g; s/@@REPOSITORY@@/REPOSITORY/g; s/@@SERVICE@@/$SERVICE/g; s/@@VERSION@@/$BUILD_NUMBER/g;" > docker-compose.yml
+
+sudo docker-compose up -d
