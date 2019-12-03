e1d5f10a03912fed818ee8c1e45472cd5b014057
bohenmian <bohenmian@gmail.com>
2019-12-03 11:10:34 +0800

    fix deploy shell error

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cat ci/docker-compose.yml | sed "s/@@HOST@@/$HOST/g; s/@@REPOSITORY@@/REPOSITORY/g; s/@@SERVICE@@/$SERVICE/g; s/@@VERSION@@/$BUILD_NUMBER/g;" > docker-compose.yml
+cat ci/docker-compose.yml | sed "s/@@HOST@@/$HOST/g; s/@@REPOSITORY@@/$REPOSITORY/g; s/@@SERVICE@@/$SERVICE/g; s/@@VERSION@@/$BUILD_NUMBER/g;" > docker-compose.yml
 
 sudo docker-compose up -d
