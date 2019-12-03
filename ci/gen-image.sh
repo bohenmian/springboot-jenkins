36765e3c70101f724374bdb1152731d6561e3f88
bohenmian <bohenmian@gmail.com>
2019-12-03 14:30:22 +0800

    fix generate docker image error

--- ci/gen-image.sh
+++ ci/gen-image.sh
@@ -6,7 +6,7 @@ REMOTE_TAG=$HOST/$REPOSITORY:$SERVICE-$BUILD_NUMBER
 username=$(cat /var/lib/jenkins/workspace/docker-registry/username)
 password=$(cat /var/lib/jenkins/workspace/docker-registry/password)
 
-docker login -u $username -p $password
+docker login -u $username -p $password hub.docker.com
 
 docker build --rm --tag $REMOTE_TAG -f ci/Dockerfile .
 docker push $REMOTE_TAG
